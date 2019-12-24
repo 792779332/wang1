60da1f7e642bb04184bb151e690ad65ef196b05a
wangjc <792779332@qq.com>
2019-12-24 15:10:50 +0800

    第二次更新

--- src/main/java/com/example/wang/web/controller.java
+++ src/main/java/com/example/wang/web/controller.java
@@ -3,6 +3,7 @@ package com.example.wang.web;
 
 import com.example.wang.Util.MessageUtil;
 import com.example.wang.bean.Message;
+import io.swagger.annotations.ApiImplicitParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,8 @@ public class controller {
 
 
     @GetMapping("/a")
-    public Message list(){
+    @ApiImplicitParam(name = "a",value = "不知道",paramType = "query",dataType = "int",required = true)
+    public Message list(int a){
 
 
         return MessageUtil.success(123);
